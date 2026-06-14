33547ae99da631a81a3d01d12011fc25aa7a1622
annqse190275 <nguyenan159246@gmail.com>
2026-06-14 11:03:40 +0700

    feat(tournament): create Interface ITournamentServices

---
+++ backend/HRTMS.Core/Interfaces/Services/ITournamentServices.cs
@@ -0,0 +1,32 @@
+﻿using HRTMS.Core.DTOs.Tournament;
+using HRTMS.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRTMS.Core.Interfaces.Services
+{
+    public interface ITournamentServices
+    {
+        // TOURNAMENT CRUD
+        Task<TournamentResponseDto> CreateTournamentAsync(CreateTournamentDto dto, int createByUserId);
+        Task<TournamentResponseDto?> GetTournamentByIdAsync(int tournamentId);
+        Task<List<TournamentResponseDto>> GetAllTournamentsAsync();
+        Task<TournamentResponseDto> UpdateTournamentAsync(int tournamentId, UpdateTournamentDto dto); 
+
+        // STATE MACHINE
+        Task<TournamentResponseDto> ChangeStatusAsync(int tournamentId, int targetStatus);
+
+        // CANCEL (TRN.10)
+        Task CancelTournamentsAsync(int tournamentId, int adminUserId);
+
+        // PRIZE DISTRIBUTION (TRN.4)
+        Task<List<PrizeDistributionResponseDto>> SetPrizeDistributionAsync(int tournamentId, SetPrizeDistributionDto dto);
+
+        // ROUND & RACE (TRN.6)
+        Task<RoundResponseDto> CreateRoundAsync(int tournamentId, CreateRoundDto dto);
+        Task<RaceResponseDto> CreateRaceAsync(int roundId, CreateRaceDto dto); 
+    }
+}
