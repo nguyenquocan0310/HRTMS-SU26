4bdff2c7209802d5ef7925d07c9dded03f693594
annqse190275 <nguyenan159246@gmail.com>
2026-06-20 15:17:34 +0700

    feat(module-c): Add Interface of Horse

---
+++ backend/HRTMS.Core/Interfaces/Services/IHorseService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRTMS.Core.Interfaces.Services
+{
+    internal interface IHorseService
+    {
+    }
+}
