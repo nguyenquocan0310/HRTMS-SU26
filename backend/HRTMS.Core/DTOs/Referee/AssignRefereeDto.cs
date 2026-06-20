dac0cbff244561eac9d88ef449d6d0ae0cf371ec
Hoai Phong <phong.no.fat@gmail.com>
2026-06-21 05:35:45 +0700

    Create: Referee Dto

--- backend/HRTMS.Core/DTOs/Referee/AssignRefereeDto.cs
+++ backend/HRTMS.Core/DTOs/Referee/AssignRefereeDto.cs
@@ -1,12 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
-namespace HRTMS.Core.DTOs.Referee
+namespace HRTMS.Core.DTOs.Referee;
+
+public class AssignRefereeDto
 {
-    internal class AssignRefereeDto
-    {
-    }
+    // Id cua Referee duoc Admin gan vao Race
+    [Required(ErrorMessage = "RefereeId is required.")]
+    public int RefereeId { get; set; }
+
+    // Vai tro cua Referee trong Race
+    // Chi chap nhan Lead Referee hoac Assistant Referee
+    [Required(ErrorMessage = "Role is required.")]
+    [RegularExpression(
+        "^(Lead Referee|Assistant Referee)$",
+        ErrorMessage = "Role must be Lead Referee or Assistant Referee.")]
+    public string Role { get; set; } = null!;
 }
