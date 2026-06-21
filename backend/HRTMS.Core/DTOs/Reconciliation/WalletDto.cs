3df8e7e96a4de6e9b2caa6d2ad17cd813c662fcf
Hoai Phong <phong.no.fat@gmail.com>
2026-06-22 00:51:54 +0700

    fix bug module Reciliation

---
+++ backend/HRTMS.Core/DTOs/Reconciliation/WalletDto.cs
@@ -0,0 +1,10 @@
+﻿namespace HRTMS.Core.DTOs.Reconciliation;
+
+public class WalletDto
+{
+    public int WalletId { get; set; }
+
+    public int Balance { get; set; }
+
+    public List<TransactionDto> Transactions { get; set; } = new();
+}
