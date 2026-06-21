3aca5768432eb14e92bcd24a0178d8ea27da5983
Hoai Phong <phong.no.fat@gmail.com>
2026-06-22 03:31:49 +0700

    Implement Medical

---
+++ backend/HRTMS.Core/DTOs/Medical/RecordPreRaceWeightDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HRTMS.Core.DTOs.Medical;
+
+public class RecordPreRaceWeightDto
+{
+    // Can nang Jockey truoc khi dua
+    [Required(ErrorMessage = "PreRaceJockeyWeight is required.")]
+    [Range(1, 300, ErrorMessage = "PreRaceJockeyWeight must be greater than 0.")]
+    public decimal PreRaceJockeyWeight { get; set; }
+}
