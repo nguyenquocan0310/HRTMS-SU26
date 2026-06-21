7c8004a1ab5b6546d19cff68f56a7c66d3c6f83e
Hoai Phong <phong.no.fat@gmail.com>
2026-06-22 02:43:48 +0700

    Create DoctorDto

---
+++ backend/HRTMS.Core/DTOs/Doctor/AssignDoctorDto.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HRTMS.Core.DTOs.Doctor;
+
+public class AssignDoctorDto
+{
+    [Required(ErrorMessage = "DoctorId is required.")]
+    public int DoctorId { get; set; }
+}
---
+++ backend/HRTMS.Core/DTOs/Doctor/DoctorAssignmentDto.cs
@@ -0,0 +1,18 @@
+﻿namespace HRTMS.Core.DTOs.Doctor;
+
+public class DoctorAssignmentDto
+{
+    public int RaceId { get; set; }
+
+    public int DoctorId { get; set; }
+
+    public string DoctorName { get; set; } = null!;
+
+    public string DoctorEmail { get; set; } = null!;
+
+    public string MedicalLicenseNumber { get; set; } = null!;
+
+    public DateTime AssignedAt { get; set; }
+
+    public DateTime? CertifiedAt { get; set; }
+}
